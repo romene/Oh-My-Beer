10bd175dbffa47027888e2ec3fa6ab299d950246
romene <rogerio.eire@gmail.com>
2019-11-29 12:27:00 +1300

    query product from contentfull

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,72 +1,62 @@
-import React from 'react'
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import { Card, Divider, Col } from "antd"
+const { Meta } = Card
 
-
-import { Card , Divider, Col} from 'antd';
-const { Meta } = Card;
-
-
-const Cards = () => {
-    return (
-      <div className="container">
-        <div className="row">
-          <Divider>
-            {" "}
-            <h2 style={{fontFamily: 'Luckiest Guy'}}>Featured</h2>
-          </Divider>
-          <Col xs={24} md={8}>
-            <Card
-              hoverable
-              style={{ maxWidth: 300, margin: "1em auto" }}
-              cover={
-                <img
-                  alt="example"
-                  src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-                />
+const Cards = ({ data }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allContentfulProduct {
+          totalCount
+          edges {
+            node {
+              contentfulid
+              title
+              description
+              createdAt
+              productImage {
+                fixed(width: 300) {
+                  ...GatsbyContentfulFixed_tracedSVG
+                }
               }
-            >
-              <Meta
-                title="Europe Street beat"
-                description="www.instagram.com"
-              />
-            </Card>
-          </Col>
-          <Col xs={24} md={8}>
-            <Card
-              hoverable
-              style={{ maxWidth: 300, margin: "1em auto" }}
-              cover={
-                <img
-                  alt="example"
-                  src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-                />
-              }
-            >
-              <Meta
-                title="Europe Street beat"
-                description="www.instagram.com"
-              />
-            </Card>
-          </Col>
-          <Col xs={24} md={8}>
-            <Card
-              hoverable
-              style={{ maxWidth: 300, margin: "1em auto" }}
-              cover={
-                <img
-                  alt="example"
-                  src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-                />
-              }
-            >
-              <Meta
-                title="Europe Street beat"
-                description="www.instagram.com"
-              />
-            </Card>
-          </Col>
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      const products = data.allContentfulProduct.edges
+
+      return (
+        <div className="container">
+          <div className="row">
+            <Divider>
+              {" "}
+              <h2 style={{ fontFamily: "Luckiest Guy" }}>Featured</h2>
+            </Divider>
+            {products.map(beer => {
+              const { contentfulid, title, description, createdAt } = beer.node
+              const productImage = beer.node.productImage.fixed
+              console.log(Image)
+              return (
+                <Col key={contentfulid} xs={24} md={8}>
+                  <Card
+                    hoverable
+                    style={{ maxWidth: 300, margin: "1em auto" }}
+                    cover={<Img alt="example" fixed={productImage} />}
+                  >
+                    <Meta title={title} description={description} />
+                  </Card>
+                </Col>
+              )
+            })}
+          </div>
         </div>
-      </div>
-    )
-}
+      )
+    }}
+  />
+)
 
 export default Cards
